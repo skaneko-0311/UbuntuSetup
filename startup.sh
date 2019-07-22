ac1625e44567926de891316d2bf56caea80bf0ea
skaneko-0311 <skaneko.0311@gmail.com>
2019-07-22 07:33:50 -0700

    Install docker

---
+++ startup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -e
+
+sudo apt-get update
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update -y
+
+sudo apt-get install docker-ce -y
+
+sudo docker -v
+
+sudo docker info
