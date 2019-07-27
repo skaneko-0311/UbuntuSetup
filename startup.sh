d7c92146212833ccc1f6c4ea109f64a9880b2c39
skaneko-0311 <skaneko.0311@gmail.com>
2019-07-26 19:20:58 -0700

    Install pyenv and python 3.7.4

--- startup.sh
+++ startup.sh
@@ -24,3 +24,41 @@ sudo apt-get install docker-ce -y
 sudo apt-get install docker-compose -y
 
 sudo apt-get install emacs25 -y
+
+mkdir $HOME/github
+
+mkdir $HOME/github/pyenv
+
+git clone https://github.com/pyenv/pyenv.git $HOME/pyenv
+
+echo 'export PYENV_ROOT="$HOME/pyenv"' >> $HOME/.profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> $HOME/.profile
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> $HOME/.profile
+
+source ~/.profile
+
+sudo apt-get install -y \
+    gcc \
+    make \
+    build-essential \
+    libssl-dev \
+    zlib1g-dev \
+    libbz2-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    wget \
+    curl \
+    llvm \
+    libncurses5-dev \
+    xz-utils \
+    tk-dev \
+    libxml2-dev \
+    libxmlsec1-dev \
+    libffi-dev \
+    liblzma-dev
+
+pyenv install -s 3.7.4
+
+pyenv local 3.7.4
+
+pyenv versions
